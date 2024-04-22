4f8723c842370a35cbd615e23d86143c7411a599
Sabira-R <123287816+Sabira-R@users.noreply.github.com>
2024-04-23 04:31:46 +0600

    Add files via upload

--- Consultation/previous.php
+++ Consultation/previous.php
@@ -1,20 +1,27 @@
 <?php
+session_start();
 require_once('DBconnect.php');
-
+$user_id=$_SESSION['user_id'];
 //Consultation table
-$sql = "SELECT c_id, faculty_id, student_id FROM consultation";
+$sql = "SELECT c_id, consultant, user_id FROM consultation where user_id=$user_id ";
 $result = $conn->query($sql);
+$user_id=$_SESSION['user_id'];
+$sql2= "SELECT user_info.name FROM user_info where user_id=$user_id";
+$result2 = $conn->query($sql2);
+$row = $result2->fetch_assoc();
+$name = $row['name'];
+echo $name;
 
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         echo "<tr>";
         echo "<td>" . $row["c_id"] . "</td>";
-        echo "<td>" . $row["faculty_name"] . "</td>";
-        echo "<td>" . $row["student_name"] . "</td>";
+        echo "<td>" . $row["consultant"] . "</td>";
+        echo "<td>" . $name . "</td>";
         
         // students review button only
         if ($_SESSION['user_id'][0] == '2') {
-            echo "<td><a href='feedback.html?c_id=" . $row["c_id"] . "&student_id=" . $_SESSION['user_id'] . "'>Review</a></td>";
+            echo "<td><a href='feedback_student.php?c_id=" . $row["c_id"] . "&student_id=" . $_SESSION['user_id'] . "'>Review</a></td>";
         } else {
             echo "<td>Not available</td>";
         }
